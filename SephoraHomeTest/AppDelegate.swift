1ab9e0ccea215c9517ddc7971a24618fe9ab4e13
양홍범 <ghdqja4005@gmail.com>
2020-04-03 11:39:22 +0900

    First Commit. Added Pods Alamofire

---
+++ SephoraHomeTest/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  SephoraHomeTest
+//
+//  Created by HongBeom Yang on 2020/04/03.
+//  Copyright © 2020 Spree. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ SephoraHomeTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SephoraHomeTest
+//
+//  Created by HongBeom Yang on 2020/04/03.
+//  Copyright © 2020 Spree. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
